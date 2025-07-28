58b89cdba8bdecbdaae154ace2a92d539152e962
jorenzprintln <jorenzlnu@gmail.com>
2025-07-28 16:46:51 +0800

    first commit

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import tailwindcss from '@tailwindcss/vite';
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: ['resources/css/app.css','resources/css/personal_web.css', 'resources/js/personal_web.js',  'resources/js/app.js'],
+            refresh: true,
+        }),
+        tailwindcss(),
+    ],
+});
